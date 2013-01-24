12bc1686261690c6a3bd45b9a2371bfc28d877ca
Erik Søe Sørensen <ess@trifork.com>
2013-01-24 18:35:52 +0100

    Quests: Adjust levels and implement sum_of_numbers.

--- src/quest_quests.erl
+++ src/quest_quests.erl
@@ -12,16 +12,16 @@ quest_list() ->
     [{any_answer,         0, 1, "Answer with any value whatsoever."},
      {any_pid,            1, 1, "Answer with a value of type pid."},
      {any_reference,      1, 1, "Answer with a value of type reference."},
-     {list_of_length_10,  1, 1, "Answer with any list of length 10."},
-     {impure_list,        2, 2, "Answer with any impure list."},
-     {answer_the_input,   2, 2, "Answer with the input given in the challenge."},
-     {sum_of_numbers,     4, 3, "Given a list of numbers, answer with their sum."},
-     {even_count,         4, 3, "Given a list of integers, answer how many of them are even."},
-     {tuple_swap,         4, 2, "Given a pair (2-tuple), answer with a pair with the same elements, but swapped."},
-     {base_7,             7, 5, "Given an integer, answer with a string containing the base 7 representation of the number."},
-     {tuple_rotate,      10, 5, "Given a tuple of an unknown arity, rotate the elements one place to the left."},
-     {primality_check,   10, 7, "Given a list of integers, answer with a list of booleans indicating whether the corresponding number is a prime."},
-     {boolean_evaluator, 15, 15,
+     {answer_the_input,   1, 1, "Answer with the input given in the challenge."},
+     {list_of_length_10,  2, 1, "Answer with any list of length 10."},
+     {impure_list,        4, 2, "Answer with any impure list."},
+     {sum_of_numbers,     8, 3, "Given a list of numbers, answer with their sum."},
+     {even_count,         8, 3, "Given a list of integers, answer how many of them are even."},
+     {tuple_swap,         8, 2, "Given a pair (2-tuple), answer with a pair with the same elements, but swapped."},
+     {base_7,            14, 5, "Given an integer, answer with a string containing the base 7 representation of the number."},
+     {tuple_rotate,      20, 5, "Given a tuple of an unknown arity, rotate the elements one place to the left."},
+     {primality_check,   20, 7, "Given a list of integers, answer with a list of booleans indicating whether the corresponding number is a prime."},
+     {boolean_evaluator, 30, 15,
       "Given a boolean expression of the grammar:\n"++
           "expr ::= a | b | c       % Variables\n"++
           "       | true | false    % Constants\n"++
@@ -61,7 +61,17 @@ answer_the_input() ->
     #quest{generate=fun()->semi_bignum() end,
            verify=fun(Input,Answer) -> Answer=:=Input end}.
 
+sum_of_numbers() ->
+    #quest{generate=fun()->[rnd_integer() || _ <- lists:seq(1,5+rnd_integer(15))] end,
+           verify=fun(Input,Answer) -> Answer==lists:sum(Input) end}.
+
 %%%==================== Common generators ==============================
+
+rnd_integer() ->
+    crypto:rand_uniform(-100,100).
+
+rnd_integer(N) ->
+    crypto:rand_uniform(1,N).
 
 semi_bignum() ->
     <<ID:64>> = crypto:rand_bytes(8),
